bcffb6b5a36671ddb55f344e67547969342526b4
ANIKETHPAWAR <Anikethpawar05@gmail.com>
2025-01-26 09:49:03 +0530

    day2

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 "use strict";
-let x = "Aniketh";
-console.log(x);
+function isLegal(age) {
+    if (age > 18) {
+        return "user is legal";
+    }
+    else {
+        return "user is underage";
+    }
+}
+console.log(isLegal(21));
--- index.ts
+++ index.ts
@@ -1,2 +1,10 @@
-let x: string = "Aniketh"
-console.log(x)
+function isLegal(age:number){
+    if (age>18){
+        return "user is legal"
+    }
+    else{
+        return "user is underage"
+    }
+}
+
+console.log(isLegal(21))
